0cf3f40a2ece2092c6089a9b0ebd994799f774ef
Aikya Halder <aikyahd@gmail.com>
2025-09-20 18:52:00 +0600

    Dynamic cart button and variation conflicts fixed

--- assets/frontend.js
+++ assets/frontend.js
@@ -8,39 +8,46 @@
 ;(function($, window, document){
 	class ProductTotalNew{
 		constructor(){
+			const self = this;
+
 			this.$working    = false;
 			this.$priceWrap  = $(document).find('.sptotal-price');
 			this.$totalPrice = this.$priceWrap.find('.total-price');
 			this.$cartBtn    = $(document).find('form.cart .single_add_to_cart_button');
 
-			$(document).ready(() => {
-				this.init();
+			$(document).ready(function(){
+				setTimeout(function(){
+					self.init();
+				}, sptotal_data.settings.delay);
 			});
-			this.$working = this.$working;
 		}
 		init(){
-			this.$working = true;
+			const self = this;
+
 			this.handleOtherTriggers();
 			this.updateTotal();
 
-			$(document).on('change input', 'form.cart .quantity .qty', () => {
-				if(!this.$working ){
-					this.$working = true;
-					this.updateTotal();
+			$(document).on('change input', 'form.cart .quantity .qty', function(){
+				console.log('qt changed');
+				if(!self.$working ){
+					self.$working = true;
+					self.updateTotal();
 				}
 			});
-			$(document).on('click', '.minus, .plus', () => {
-				if(!this.$working ){
-					this.$working = true;
-					this.updateTotal();
+			$(document).on('click', '.minus, .plus', function(){
+				if(!self.$working ){
+					self.$working = true;
+					self.updateTotal();
 				}
 			});
-			$(document).on('click', '.sptotal-cart-btn', () => {
-				if(!this.$working ){
-					this.$working = true;
-					this.$cartBtn.trigger('click');
+			$(document).on('click', '.sptotal-cart-btn', function(){
+				if(!self.$working ){
+					self.$working = true;
+					self.$cartBtn.trigger('click');
 				}
 			});
+
+			this.persistantEventHandler();
 		}
 		handleOtherTriggers(){
 			const self = this;
@@ -55,7 +62,6 @@
 					}
 				});
 			});
-			// this.$working = this.$working;
 
 			// handle YITH Dynamic Pricing.
 			const tables = $(document).find('.ywdpd-quantity-table');
@@ -71,26 +77,34 @@
 					});
 				});
 			}
-			// this.$working = self.$working;
 		}
-		updateTotal(){
-			this.loaderAnimation(this.$priceWrap, 'start');
-			setTimeout(() => {
-				const total = this.calculateTotalPrice();
-				this.$totalPrice.text(this.parsePrice(total, 'front'));
 
-				this.loaderAnimation(this.$priceWrap, 'stop');
-				this.$working = false;
-			}, parseInt(sptotal_data['settings']['delay']));
-			// this.$working = this.$working;
+
+
+		updateTotal(){
+			const self = this;
+
+			let disableCart = false;
+			this.loaderAnimation(this.$priceWrap, 'start');
+			setTimeout(function(){
+				const total = self.calculateTotalPrice();
+				if(total === 0) disableCart = true;
+				self.$totalPrice.text(self.parsePrice(total, 'front'));
+
+				self.loaderAnimation(self.$priceWrap, 'stop');
+				self.$working = false;
+			}, parseInt(sptotal_data.settings.delay));
+
+			// console.log('[updateTotal]');
+			this.cartBtnHandler(disableCart);
 		}
 		calculateTotalPrice(){
 			const self = this;
 
-			let total        = 0.00;
-			const quantities = $(document).find('form.cart .quantity .qty');
-			if(quantities.length > 1){
-				$.each(quantities, function (index, item){
+			let total  = 0.00;
+			const qtys = $(document).find('form.cart .quantity .qty');
+			if(qtys.length > 1){
+				$.each(qtys, function (index, item){
 					const wrap  = $(item).closest('.woocommerce-grouped-product-list-item').find('.woocommerce-grouped-product-list-item__price');
 					const price = self.parsePrice(self.getPriceText(wrap), 'back');
 					const qty   = $(item).val();
@@ -108,33 +122,27 @@
 				wrap = wrap.length === 0 || wrap.text().length === 0 ? $(document).find('p.price') : wrap;
 
 				const price = this.parsePrice(this.getPriceText(wrap), 'back');
-				const qty   = quantities.val();
+				const qty   = qtys.val();
 				if(qty.length > 0 && price.length > 0){
 					total += (parseFloat(price) * parseInt(qty));
 				}
 			}
-			this.$working = this.$working;
 			return this.checkForIssues() ? 0.00 : total;
 		}
-		checkForIssues(){
-			let issues = false;
-			// check if all variations are selected.
-			const wrap = this.getVariationsWrap();
-			if(wrap.length){
-				let total = 0, selected = 0;
-				wrap.find('select[name^="attribute_"]').each(function (index, item){
-					total++;
-					if($(item).find('option:selected').val().length){
-						selected++;
-					}
-				});
-				// console.log('checkForIssues', total, selected );
-
-				if(total !== selected){
-					issues = true;
-				}
+		parsePrice(price, flow='front'){
+			if(price === undefined){
+				return 0.0;
 			}
-			return issues;
+			if(flow === 'front'){ // parse price to text price.
+				price = parseFloat(price).toFixed(sptotal_data['dp']);
+				price = price.replace('.', sptotal_data['ds']);
+				price = price.replace(/\B(?=(\d{3})+(?!\d))/g, sptotal_data['ts']);
+			} else { // parse text to find price.
+				price = price.replace(/[^\d.,]/g, ''); // filter out number parts from string.
+				price = price.replace(sptotal_data['ts'], ''); // thousand separator.
+				price = price.replace(sptotal_data['ds'], '.'); // decimal separator.
+			}
+			return price;
 		}
 		getPriceText(wrap){
 			if(wrap === undefined || 0 === wrap.length){
@@ -152,31 +160,49 @@
 
 			return text;
 		}
-		parsePrice(price, flow='front'){
-			if(price === undefined){
-				return 0.0;
+		checkForIssues(){
+			let hasIssue = false;
+			// check if all variations are selected.
+			const wrap = this.getVariationsWrap();
+			if(wrap.length){
+				let total = 0, selected = 0;
+				wrap.find('select[name^="attribute_"]').each(function (index, item){
+					total++;
+					if($(item).find('option:selected').val().length > 0){
+						selected++;
+					}
+				});
+				// console.log('has issue?', total, selected );
+				if(total !== selected) hasIssue = true;
 			}
-			if(flow === 'front'){ // parse price to text price.
-				price = parseFloat(price).toFixed(sptotal_data['dp']);
-				price = price.replace('.', sptotal_data['ds']);
-				price = price.replace(/\B(?=(\d{3})+(?!\d))/g, sptotal_data['ts']);
-			} else { // parse text to find price.
-				price = price.replace(/[^\d.,]/g, ''); // filter out number parts from string.
-				price = price.replace(sptotal_data['ts'], ''); // thousand separator.
-				price = price.replace(sptotal_data['ds'], '.'); // decimal separator.
+
+			// check if variation add to cart button is enabled or not.
+			const cartBtn = $(document).find('.woocommerce-variation-add-to-cart');
+			// console.log('cart btn', cartBtn);
+			if(cartBtn && cartBtn.hasClass('woocommerce-variation-add-to-cart-disabled')){
+				// console.log('cart btn disabled');
+				return true;
 			}
-			return price;
+			return hasIssue;
+		}
+		getVariationsWrap(){
+			let wrap = $(document).find('table.variations');
+			if(wrap.length === 0){
+				wrap = $(document).find('.variations_form');
+			}
+			return wrap.length > 0 ? wrap : false;
 		}
 
 		loaderAnimation(item, action = 'start'){
 			const key     = 'sptotal';
-			const aniWrap = item.find(`.${key}-loading`); // animation wrapper.
+			const aniWrap = $(document).find(`.sptotal .${key}-loading`); // animation wrapper.
+			// console.log('[spinner:item]', item);
 			if(action === 'start'){
 				if(!item.hasClass(`${key}-disable`)){
 					item.addClass(`${key}-disable`);
 				}
 				if(typeof aniWrap === undefined || aniWrap.length === 0){
-					item.append(`<div class="${key}-loading"></div>`);
+					item.before(`<div class="${key}-loading"></div>`);
 				}
 			} else {
 				item.removeClass(`${key}-disable`);
@@ -197,12 +223,50 @@
 			}
 			return swatches;
 		}
-		getVariationsWrap(){
-			let wrap = $(document).find('table.variations');
-			if(wrap.length === 0){
-				wrap = $(document).find('.variations_form');
+
+
+
+
+
+		persistantEventHandler(){
+			const self = this;
+
+			$(document).on('change', '.variations select', function(){
+				// console.log('[select:changed]');
+				// console.log('[select:changed]', $(this).attr('name'));
+				if($(this).find('option:selected').val().length === 0){
+					self.resetTotal();
+					self.cartBtnHandler(true);
+				}else{
+					self.cartBtnHandler();
+				}
+			});
+			$(document).on('click', 'a.reset_variations', function(){
+				// console.log('[reset:clicked]');
+				self.resetTotal();
+				self.cartBtnHandler(true);
+			});
+		}
+		resetTotal(){
+			this.$totalPrice.text(this.parsePrice(0, 'front'));
+		}
+		cartBtnHandler(override = false){
+			const totalCartBtn = $(document).find('.sptotal-cart-btn');
+			if(!totalCartBtn) return;
+
+			if(override && !totalCartBtn.hasClass('sptotal-disable')){
+				// console.log('[override:cart - disable]');
+				totalCartBtn.addClass('sptotal-disable');
 			}
-			return wrap.length > 0 ? wrap : false;
+			if(override) return;
+
+			setTimeout(function(){
+				const cartBtn   = $(document).find('.woocommerce-variation-add-to-cart');
+				const isDisable = cartBtn && cartBtn.hasClass('woocommerce-variation-add-to-cart-disabled');
+				// console.log('cart btn disable?', isDisable);
+				if(isDisable && !totalCartBtn.hasClass('sptotal-disable')) totalCartBtn.addClass('sptotal-disable');
+				else totalCartBtn.removeClass('sptotal-disable');
+			}, sptotal_data.settings.delay);
 		}
 	}
 	new ProductTotalNew();
